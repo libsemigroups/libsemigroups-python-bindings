b8c5440d65e8ecc42ebb22b38315382bcc114208
James Mitchell <jdm3@st-and.ac.uk>
2017-04-21 15:06:26 -0400

    Only add -mmacosx... compiler flag on MacOSX

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
 # -*- coding: utf-8 -*-
 
+import codecs
+from sys import platform
+from os import path
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-from codecs import open
-from os import path
 
-#from setuptools.dist import Distribution
-#from Cython.Distutils import build_ext
-#Distribution(dict(setup_requires='Cython'))
+# from setuptools.dist import Distribution
+# from Cython.Distutils import build_ext
+# Distribution(dict(setup_requires='Cython'))
 
 here = path.abspath(path.dirname(__file__))
 
+extra_compile_args = ["-std=c++11"]
+if platform == 'darwin':
+    extra_compile_args.append("-mmacosx-version-min=10.9")
+
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with codecs.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    version = "0.2.2",
-    name = "libsemigroups-python-bindings",
+    version="0.2.2",
+    name="libsemigroups-python-bindings",
     description='Python bindings for the libsemigroups mathematics library',
     long_description=long_description,
-    url='https://github.com/james-d-mitchell/libsemigroups/tree/master/python-bindings',
+    url=('https://github.com/james-d-mitchell/libsemigroups/' +
+         'tree/master/python-bindings'),
     author='James Mitchell and Nicolas M. Thiéry',
     author_email='TODO',
     license='GPLv3',
@@ -35,17 +41,17 @@ setup(
     ],
     keywords='Mathematics, semigroup theory',
 
-    ext_modules = cythonize([
+    ext_modules=cythonize([
         Extension("semigroups",
-                      sources=["semigroups.pyx", "semigroups_cpp.cpp"],
-                      depends=["semigroups_cpp.h", "semigroups_cpp.pxd"],
-                      libraries=["semigroups"],
-                      language="c++",             # generate C++ code
-                      extra_compile_args=["-std=c++11",
-                                          "-mmacosx-version-min=10.9"],
-                      )]),
+                  sources=["semigroups.pyx", "semigroups_cpp.cpp"],
+                  depends=["semigroups_cpp.h", "semigroups_cpp.pxd"],
+                  libraries=["semigroups"],
+                  language="c++",             # generate C++ code
+                  extra_compile_args=extra_compile_args
+                 )]),
 
-    #setup_requires=['pytest-runner', 'pytest-cython'], # temporarilly disabled to build conda package
+    # setup_requires=['pytest-runner', 'pytest-cython'], # temporarilly
+    # disabled to build conda package
     tests_require=['pytest'],
 )
 
